56df57e92e04db51e0820c63480405feb8069773
esanchez2898 <esanchez2898@outlook.com>
2024-10-23 20:33:26 -0600

    Add loading message in todo and register

--- public/login.js
+++ public/login.js
@@ -53,6 +53,16 @@ registerForm.addEventListener('submit', (e) => {
 
     if (!validateForm(userEmail, userPassword)) return;  // Validate the form. If validation fails, stop here.
 
+    // Mostrar mensaje de "Cargando..."
+    Swal.fire({
+        title: 'Loading...',
+        text: 'Please wait while we log you in',
+        allowOutsideClick: false,  // No permite cerrar la alerta haciendo clic fuera
+        didOpen: () => {
+            Swal.showLoading();  // Muestra el spinner de carga
+        }
+    });
+
     // Send a POST request to the server to register the new user
     fetch('https://todolist-j854.onrender.com/register', {
         method: 'POST',
@@ -62,14 +72,17 @@ registerForm.addEventListener('submit', (e) => {
         .then(response => response.json())  // Convert the server response to JSON
         .then(data => {  // Handle the response
             if (data.message) {
+                Swal.close();
                 Swal.fire('Success', data.message, 'success').then(() => {
                     window.location.href = 'index.html';  // If registration is successful, redirect to login
                 });
             } else {
+                Swal.close();
                 toastr.error(data.error);
             }
         })
         .catch(error => {  // Handle any errors that occur during the request
+            Swal.close();
             toastr.error('Something went wrong with the connection');
             console.error('Error:', error);
         });
--- public/todo.js
+++ public/todo.js
@@ -46,6 +46,19 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     logout.addEventListener('click', () => {
+
+        // Mostrar mensaje de "Cargando..."
+        Swal.fire({
+            title: 'Loading...',
+            text: 'Please wait while we log you in',
+            allowOutsideClick: false,  // No permite cerrar la alerta haciendo clic fuera
+            didOpen: () => {
+                Swal.showLoading();  // Muestra el spinner de carga
+            }
+        });
+
+
+
         fetch('https://todolist-j854.onrender.com/logout', {  // Cambiar aquí
             method: 'POST',
             headers: {
@@ -55,10 +68,15 @@ document.addEventListener('DOMContentLoaded', () => {
         })
             .then(response => response.json())
             .then(data => {
+                Swal.close();
                 console.log(data);
                 window.location.href = 'index.html';
             })
-            .catch(error => console.error('Error:', error));
+            .catch(error => {
+                Swal.close();
+                console.error('Error:', error)
+            }
+            );
     });
 
     // POST
@@ -84,106 +102,106 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch('https://todolist-j854.onrender.com' + route, {
             credentials: 'include' // Incluye las cookies en la solicitud
         })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            newItemsList.innerHTML = '';
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                newItemsList.innerHTML = '';
 
-            const items = data.jsonResponse;
+                const items = data.jsonResponse;
 
-            items.forEach(item => {
-                const div = document.createElement('div');
-                const inputText = document.createElement("input");
-                inputText.type = "text";
-                inputText.id = "miInput";
-                inputText.value = `${item.todo}`;
-                inputText.setAttribute('readonly', true); // Hace el input de solo lectura
+                items.forEach(item => {
+                    const div = document.createElement('div');
+                    const inputText = document.createElement("input");
+                    inputText.type = "text";
+                    inputText.id = "miInput";
+                    inputText.value = `${item.todo}`;
+                    inputText.setAttribute('readonly', true); // Hace el input de solo lectura
 
-                const deleteButton = document.createElement('button');
-                deleteButton.textContent = '✖';
-                deleteButton.addEventListener('click', () => deleteItem(item.id));
+                    const deleteButton = document.createElement('button');
+                    deleteButton.textContent = '✖';
+                    deleteButton.addEventListener('click', () => deleteItem(item.id));
 
-                const updateButton = document.createElement('button');
-                updateButton.textContent = '✎';
-                updateButton.addEventListener('click', () => updateItem(item.id, prompt('Enter new name:')));
+                    const updateButton = document.createElement('button');
+                    updateButton.textContent = '✎';
+                    updateButton.addEventListener('click', () => updateItem(item.id, prompt('Enter new name:')));
 
-                const checkbox = document.createElement('input');
-                checkbox.type = 'checkbox';
+                    const checkbox = document.createElement('input');
+                    checkbox.type = 'checkbox';
 
-                checkbox.addEventListener('click', () => {
-                    if (checkbox.checked) {
+                    checkbox.addEventListener('click', () => {
+                        if (checkbox.checked) {
+                            inputText.classList.add('itemCompleted');
+                            updateStatus('completed', item.id);
+                        } else {
+                            inputText.classList.remove('itemCompleted');
+                            updateStatus('active', item.id);
+                        }
+                    });
+
+                    if (item.status === 'completed') {
                         inputText.classList.add('itemCompleted');
-                        updateStatus('completed', item.id);
-                    } else {
-                        inputText.classList.remove('itemCompleted');
-                        updateStatus('active', item.id);
+                        checkbox.checked = true;
                     }
+
+                    div.appendChild(deleteButton);
+                    div.appendChild(updateButton);
+                    div.appendChild(checkbox);
+                    div.appendChild(inputText);
+                    newItemsList.appendChild(div);
                 });
 
-                if (item.status === 'completed') {
-                    inputText.classList.add('itemCompleted');
-                    checkbox.checked = true;
-                }
+                // Footer section with filters
+                const div = document.createElement('div');
+                div.id = "lastSection";
 
-                div.appendChild(deleteButton);
-                div.appendChild(updateButton);
-                div.appendChild(checkbox);
-                div.appendChild(inputText);
+                const divItemsLeft = document.createElement('div');
+                divItemsLeft.id = "itemsLeft";
+
+                const span = document.createElement('span');
+                const buttonAll = document.createElement('button');
+                buttonAll.classList.add('lastSectionBotton');
+                const buttonActive = document.createElement('button');
+                buttonActive.classList.add('lastSectionBotton');
+                const buttonCompleted = document.createElement('button');
+                buttonCompleted.classList.add('lastSectionBotton');
+
+                span.textContent = items.length; // Se asume que items es un array
+                divItemsLeft.textContent = ' items left';
+                divItemsLeft.prepend(span);
+
+                buttonAll.textContent = 'All';
+                buttonAll.addEventListener('click', () => {
+                    route = '/items';
+                    loadAllItems(route);
+                });
+
+                buttonActive.textContent = 'Active';
+                buttonActive.addEventListener('click', () => {
+                    route = '/items?status=active';
+                    loadAllItems(route);
+                });
+
+                buttonCompleted.textContent = 'Completed';
+                buttonCompleted.addEventListener('click', () => {
+                    route = '/items?status=completed';
+                    loadAllItems(route);
+                });
+
+                div.appendChild(divItemsLeft);
+                div.appendChild(buttonAll);
+                div.appendChild(buttonActive);
+                div.appendChild(buttonCompleted);
                 newItemsList.appendChild(div);
-            });
 
-            // Footer section with filters
-            const div = document.createElement('div');
-            div.id = "lastSection";
-
-            const divItemsLeft = document.createElement('div');
-            divItemsLeft.id = "itemsLeft";
-
-            const span = document.createElement('span');
-            const buttonAll = document.createElement('button');
-            buttonAll.classList.add('lastSectionBotton');
-            const buttonActive = document.createElement('button');
-            buttonActive.classList.add('lastSectionBotton');
-            const buttonCompleted = document.createElement('button');
-            buttonCompleted.classList.add('lastSectionBotton');
-
-            span.textContent = items.length; // Se asume que items es un array
-            divItemsLeft.textContent = ' items left';
-            divItemsLeft.prepend(span);
-
-            buttonAll.textContent = 'All';
-            buttonAll.addEventListener('click', () => {
-                route = '/items';
-                loadAllItems(route);
-            });
-
-            buttonActive.textContent = 'Active';
-            buttonActive.addEventListener('click', () => {
-                route = '/items?status=active';
-                loadAllItems(route);
-            });
-
-            buttonCompleted.textContent = 'Completed';
-            buttonCompleted.addEventListener('click', () => {
-                route = '/items?status=completed';
-                loadAllItems(route);
-            });
-
-            div.appendChild(divItemsLeft);
-            div.appendChild(buttonAll);
-            div.appendChild(buttonActive);
-            div.appendChild(buttonCompleted);
-            newItemsList.appendChild(div);
-
-            if (route === '/items') {
-                buttonAll.classList.add('botonActivo');
-            } else if (route === '/items?status=active') {
-                buttonActive.classList.add('botonActivo');
-            } else if (route === '/items?status=completed') {
-                buttonCompleted.classList.add('botonActivo');
-            }
-        })
-        .catch(error => console.error('Error:', error));
+                if (route === '/items') {
+                    buttonAll.classList.add('botonActivo');
+                } else if (route === '/items?status=active') {
+                    buttonActive.classList.add('botonActivo');
+                } else if (route === '/items?status=completed') {
+                    buttonCompleted.classList.add('botonActivo');
+                }
+            })
+            .catch(error => console.error('Error:', error));
     }
 
     // DELETE
@@ -201,7 +219,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // PATCH STATUS
-    function updateStatus(status, id) {  
+    function updateStatus(status, id) {
         fetch(`https://todolist-j854.onrender.com/items/${id}/status`, {  // Cambiar aquí
             method: 'PATCH',
             headers: {
@@ -236,5 +254,5 @@ document.addEventListener('DOMContentLoaded', () => {
                 })
                 .catch(error => console.error('Error:', error));
         }
-    }    
+    }
 });
